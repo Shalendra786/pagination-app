fb330731dc18c7b8cb2f8f10295a7870f7fc4f8c
shalendra786 <shalendram@witmates.com>
2022-05-13 18:33:06 +0530

    pagination done

--- src/components/pagination.jsx
+++ src/components/pagination.jsx
@@ -1,23 +1,11 @@
 import React, { useState } from 'react';
-import useInfiniteScroll from "./useInfiniteScroll";
 
 const List = () => {
-  const [listItems, setListItems] = useState( );
-  const [isFetching, setIsFetching] = useInfiniteScroll(fetchMoreListItems);
 
-  function fetchMoreListItems() {
-    setTimeout(() => {
-      setListItems(prevState => ([...prevState, ...Array.from(Array(20).keys(), n => n + prevState.length + 1)]));
-      setIsFetching({...listItems.concat});
-    }, 1000);
-  }
 
   return (
     <>
-      <ul className="list-group mb-2">
-        {listItems.map(listItem => <li className="list-group-item">List Item {listItem}</li>)}
-      </ul>
-      {isFetching && 'Fetching more list items...'}
+      
     </>
   );
 };
